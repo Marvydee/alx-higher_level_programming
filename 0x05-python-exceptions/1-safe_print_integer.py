01a3fa4be3cad982a61784a84dde6b27e27de063
Marvydee <marvydee1@gmail.com>
2024-01-22 22:46:28 +0100

    a function that prints x elements of a list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -7,19 +7,17 @@ def safe_print_integer(value):
     except (ValueError, TypeError):
         return False
 
-if __name__ == "__main__":
-    value = 89
-    has_been_print = safe_print_integer(value)
+    if __name__ == "__main__":
+        value = 89
+        has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
-
-    value = -89
-    has_been_print = safe_print_integer(value)
+        value = -89
+        has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
-
-    value = "School"
-    has_been_print = safe_print_integer(value)
+        value = "School"
+        has_been_print = safe_print_integer(value)
     if not has_been_print:
         print("{} is not an integer".format(value))
 
