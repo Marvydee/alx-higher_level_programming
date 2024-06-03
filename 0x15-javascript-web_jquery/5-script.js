d8952e81b66fe98a8044899b6af0429bd8e2966a
Marvydee <marvydee1@gmail.com>
2024-06-03 21:34:08 +0100

    add new list

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+// Script to add a <li> element to a list when the user clicks on the tag DIV#add_item
+
+$(document).ready(function(){
+  $('#add_item').click(function(){
+    $('ul.my_list').append('<li>Item</li>');
+  });
+});
